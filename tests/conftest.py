e70c52e25e1660f1aa69807d400053642fafff51
olzhasar <o.arystanov@gmail.com>
2021-07-08 23:53:06 +0600

    Updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,19 @@ import pytest
 from api import create_app
 
 
-@pytest.fixture
-def client():
+@pytest.fixture(scope="session")
+def app():
     app = create_app()
 
+    from api.models import db
+
+    with app.app.app_context():
+        db.create_all()
+
+    yield app
+
+
+@pytest.fixture(scope="session")
+def client(app):
     with app.app.test_client() as client:
         yield client
