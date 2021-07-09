feb90683756deff7c177dc3d4b68a999a494a07e
olzhasar <o.arystanov@gmail.com>
2021-07-09 17:13:23 +0600

    Designed Product, ProductList, ProductPrice models

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
+    avatar_url = db.Column(db.String, nullable=True)
 
     def set_password(self, raw_password: str):
         salt = bcrypt.gensalt()
@@ -15,3 +16,32 @@ class User(db.Model):
 
     def check_password(self, password: str):
         return bcrypt.checkpw(password.encode(), self.password.encode())
+
+
+class ProductList(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", backref=db.backref("product_lists", lazy=True))
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    sku = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=True)
+    product_list_id = db.Column(
+        db.Integer, db.ForeignKey("product_list.id"), nullable=False
+    )
+    product_list = db.relationship(
+        "ProductList", backref=db.backref("products", lazy=True)
+    )
+
+    __table_args__ = (db.UniqueConstraint("sku", "product_list_id"),)
+
+
+class ProductPrice(db.Model):
+    value = db.Column(db.Integer, nullable=False)
+    currency_code = db.Column(db.String(4), nullable=False)
+
+    __table_args__ = (db.PrimaryKeyConstraint("value", "currency_code"),)
