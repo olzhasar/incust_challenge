6be8d9ad205414dd785b2621a0a29c5370e469e9
olzhasar <o.arystanov@gmail.com>
2021-07-10 01:24:53 +0600

    Updated models tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,26 @@
-from api.models import User
+import pytest
+from api.models import User, db
+
+from tests import factories
 
 
-def test_user(client, user_1):
-    user = User.query.first()
+@pytest.fixture
+def user():
+    user = factories.UserFactory()
+    user.set_password("123qweasd")
 
-    assert user == user_1
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+def test_user_persists(user):
+    from_db = User.query.filter_by(username=user.username).one_or_none()
+
+    assert from_db
+
+
+def test_user_password(user):
     assert not user.check_password("zxcasd123")
     assert user.check_password("123qweasd")
